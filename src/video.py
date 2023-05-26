82aca0c035de7549ef7c97a1a5fdab7bb965a10b
LagManKom <filagoh@yandex.ru>
2023-05-26 19:48:04 +0300

    finaly

--- src/video.py
+++ src/video.py
@@ -12,37 +12,30 @@ class Video:
         self.video_id: str = video_id
 
         try:
-            response = requests.get(f'https://www.youtube.com/{self.video_id}')
-            if response.status_code == 404:
-                raise HttpError
-            # requests.get()
+            self.__youtube = build('youtube', 'v3', developerKey=self.__apikey)
+            video = self.__youtube.captions().list(videoId=self.video_id, part='snippet').execute()
 
         except HttpError:
-            self.video_title = None
+            self.title = None
             self.video_url = None
             self.view_count = None
             self.like_count = None
 
         else:
-            self.__youtube = build('youtube', 'v3', developerKey=self.__apikey)
             self.__video_response = self.__youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
                                                                  id=video_id
                                                                  ).execute()
-            self.video_title: str = self.__video_response['items'][0]['snippet']['title']  # название видео
+            self.title: str = self.__video_response['items'][0]['snippet']['title']  # название видео
             self.video_url = f"https://www.youtube.com/watch?v={self.video_id}"  # ссылка на видео
             self.view_count: int = self.__video_response['items'][0]['statistics']['viewCount']  # количество просмотров
             self.like_count: int = self.__video_response['items'][0]['statistics']['likeCount']  # количество лайков
 
     def __str__(self):
-        return self.video_title
+        return self.title
 
     def _printj(self) -> None:
         """Выводит словарь в json-подобном удобном формате с отступами"""
         print(json.dumps(self.__video_response, indent=2, ensure_ascii=False))
-
-    # @property
-    # def response(self):
-    #     return self.__video_response
 
 
 class PLVideo(Video):
